bb8175285a0827e8df37fd7e7918e67b62e7cc42
Vizend <calvin.luzendra@gmail.com>
2026-05-20 18:47:42 +0700

    buat file request

--- app/Http/Requests/StoreAssetRequest.php
+++ app/Http/Requests/StoreAssetRequest.php
@@ -12,18 +12,80 @@ class StoreAssetRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
+        // return auth()->user()->role === 'admin'; (misal hanya admin yang bisa membuat asset)
+        //kalau sudah ada middleware role, bisa langsung return true karena sudah pasti usernya terautentikasi
     }
 
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, ValidationRule|array<mixed>|string>
      */
     public function rules(): array
     {
         return [
             //
+            'category_id' => ['required', 'exists:categories,id'],
+
+            'asset_code' => [
+                'required',
+                'string',
+                'max:100',
+                'unique:assets,asset_code'
+            ],
+
+            'name' => [
+                'required',
+                'string',
+                'max:255'
+            ],
+
+            'brand' => [
+                'nullable',
+                'string',
+                'max:255'
+            ],
+
+            'model' => [
+                'nullable',
+                'string',
+                'max:255'
+            ],
+
+            'serial_no' => [
+                'nullable',
+                'string',
+                'max:255',
+                'unique:assets,serial_no'
+            ],
+
+            'purchase_date' => [
+                'nullable',
+                'date'
+            ],
+
+            'condition' => [
+                'required',
+                'in:available,borrowed,maintenance,retired'
+            ],
+
+            'location' => [
+                'nullable',
+                'string',
+                'max:255'
+            ],
+
+            'notes' => [
+                'nullable',
+                'string'
+            ],
+
+            'photo' => [
+                'nullable',
+                'image',
+                'mimes:jpeg,png,jpg,webp',
+                'max:2048'
+            ],
         ];
     }
 }
